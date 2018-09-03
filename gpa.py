8b6f5e15338c2b77ea50223a8d2eca2543055385
Kevin Lee <b03902086@ntu.edu.tw>
2018-09-04 01:02:42 +0800

    fixes grade F bug

--- gpa.py
+++ gpa.py
@@ -22,7 +22,8 @@ def get_gpa(url):
         'A+': 4.3, 'A': 4.0, 'A-': 3.7,
         'B+': 3.3, 'B': 3.0, 'B-': 2.7,
         'C+': 2.3, 'C': 2.0, 'C-': 1.7,
-        'F': 0.0, 'X': 0.0
+        'F': 0.0, '(F )': 0.0, 'X': 0.0
+        # 'F' is kept here in case the system changes '(F )' to 'F' in the future...
     }
 
     df = df.loc[df['成績'].isin(grade2point)]
